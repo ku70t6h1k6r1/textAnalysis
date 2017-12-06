9ca71aea88dc29a711db955f01034abd1dff4757
ku70t6h1k6r1 <uju5150@gmail.com>
2017-12-07 01:05:05 +0900

    fix for sql

--- exec.rb
+++ exec.rb
@@ -1,9 +1,17 @@
 # -*- coding: utf-8 -*-
 require 'natto'
 
-txt = '明日は明日の風が吹く'
+txt = 'うむ、動けよ、動け、動いておくんなまし。なんじはまさに例えばであるように思うなりにけり安倍晋三総理大臣。えーと、トホホ'
+serial = 1000
 
 natto = Natto::MeCab.new
+i = 0
 natto.parse(txt) do |n|
-  puts "#{n.surface}: #{n.feature}"
+  feature = []
+  feature = n.feature.split(",")
+  feature = feature.slice(0..3).select{|x| x != "*"}
+  feature = feature.join(",")
+
+  puts "#{serial}/#{i}/#{n.surface}/#{feature}"
+  i = i + 1
 end
